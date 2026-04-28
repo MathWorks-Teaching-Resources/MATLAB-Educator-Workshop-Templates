4a83fa3de5c69ca91765f2959ded7d8a52745e10
Emma Smith Zbarsky <esmithzb@mathworks.com>
2026-04-28 14:21:29 -0400

    Switch to .m live script

---
+++ TeachingActivity.m
@@ -0,0 +1,46 @@
+%[text] %[text:anchor:T_6033A41B] # Teaching Activity Title 
+%[text] Author: ADD YOUR NAME HERE AND AFFILIATION 
+%[text] %[text:anchor:H_5FBF563A] ## Abstract 
+%[text] *Please provide a brief description of your activity or assignment and its results. Be sure to include keywords to help others find your materials using our search/browse features.* 
+%[text:tableOfContents]{"heading":"**Table of Contents** "}
+%[text] %[text:anchor:H_16CC76B8] ## Learning Objectives 
+%[text] *Describe the objectives of the activity: What concepts, content, and/or skills should students learn from this activity?* 
+%%
+%[text] %[text:anchor:H_29681493] ## Before You Begin 
+%[text] This MATLAB computational notebook, or Live Script, is designed to be used with the code visible and the results displayed in the notebook. On the MATLAB Options (Toolstrip) tab, in the View section, select Output Inline. Alternatively, select  **Output inline**  using the ![](text:image:3b65) icon in the right corner of the Live Script editor. 
+%[text] ![](text:image:11a3) Interacting with this Live Script will allow you to become familiar with MATLAB as concepts and commands are introduced together. If you need more training, consider taking the [MATLAB Onramp](https://matlabacademy.mathworks.com/details/matlab-onramp/gettingstarted) course, a 2-hour online tutorial that teaches the basics of MATLAB. 
+%[text] ![](text:image:1d94) For an optimal experience, follow the instructions and steps in the sequence indicated. Move on to a new section only after completing the previous one. Some sections depend on variables created in previous sections and will generate errors if executed out of order. 
+%[text] In this notebook you will have the opportunity to explore the visualization of concepts introduced, but you will also be able to use exercises that will allow you to interact with the code and challenge your understanding of those concepts and your instructor can use these to grade and verify completion. 
+%%
+%[text] %[text:anchor:H_DC039D01] ## Section 1: Concepts 
+%[text] In this section add context that introduces the concepts that will be covered in this activity 
+% Cell to add code
+%%
+%[text] %[text:anchor:H_A9973EF3] ## Section 2: Exercise 1 
+%[text] In this section add activities that allow the student to practice what they have learned 
+% Cell to add code
+%%
+%[text] %[text:anchor:H_697D4BF4] ## Section 3: Exercise 2 
+%[text] In this section add activities that allow the student to practice what they have learned 
+% Cell to add code
+%%
+%[text] %[text:anchor:H_8C0E3712] ## Additional Resources 
+%[text] In this section share additional links or references for educators and students who use this material 
+%%
+%[text] %[text:anchor:H_0DC4CD4C] ## References 
+%[text] In this section, reference materials or resources used in the creation of this material 
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"inline","rightPanelPercent":40}
+%---
+%[text:image:3b65]
+%   data: {"align":"baseline","height":21,"src":"data:image\/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAA1CAYAAAA+qNlvAAABhElEQVR4Ae2aQW6DMBREc5YscpcegyvAJVh00U1OwJJtr9PegvVvJ5KlwelXPxY2NvmWLBzHjB\/jcVAEl2VZpMV6aREazOcBn+dZ+r6XruuqqmACW0jIynF8URtwzBPgV+DDMFQPDsanjPPVSWWF2Ry8xOIkOf5xu8p\/NTe8g8crUKXjuaEs+klRsQjnHuPgVofZKa1t0eJzi9yAeEKt7eBwhss4jpJa2WXW5H5NWxtvjoombOlnQA1E09HGnx+cr3xr2+K4RZN1zI5bhLUxPOE0TY9hOHK\/di738\/gi4DHkX58ZUGsXBwdIDBsg0G8t4RwcizgewGL4LdDQOAwckwf4rdDJ4Lf3L9mrvg13sxaAQ0lyfC\/orToBGsfXBf\/8FslZeVV2dTwnNLQdHMvFLrjjyl5hkzzjcMOj4lH5fZLFt1XeGLxhPCoeFWNUODa52xxXjrH5H1BuQE3\/dcH5yo9qJ0XlKFie95zgzT4Sb\/YlBPw+Ar651z4A3kpdvfbRCjQ4fwBadyemuKnIVAAAAABJRU5ErkJggg==","width":19}
+%---
+%[text:image:11a3]
+%   data: {"align":"baseline","height":24,"src":"data:image\/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAsCAYAAAAXb\/p7AAADlklEQVR42u1YS0hUYRQejR4UvR+SvaCyWiS2DIoCKWjVYzG2SsYWQ+a8HRwXFkOgqdGLSoSeqyBaJUGLCKGkNpGtgiyScnpYBpaU5au+E+eH03Wu93rvP2OEFw46955z\/u8\/7\/\/3eDQ+4XD4Eui65199AK4d1DEJcBLgfw0wGo2ujkQi1yoqKlY5AFgDqnWw5lpaMxaLrbBkDgaDm7HIEKjTCcjxPqFQqABrdYEGsHaRXaESCAyCXpNFMwwuReDw\/77xusurQGJna0ysPR2K94CnGdQKesnUyu92J5PJaelk4c51Chzcu9dp0O9nkO3yvd\/vnwqlEbzvAf2yoB5sIkwyBt0doJ+0Qbdu2AXyC8UrOVuNQH5Q3IJegfrTfH8ikwC\/A9hjsdaYCQQCG6D4vVj0DShB7+HKXMGaQ\/EFADEGrPhJdn1GAhoZvRDKX4jFTvt8vhlWchSD4D0OGiE5AH9eXl4+PxNF+IoCB8sccyBfreQB8qJWcAC0CYqHeYE2cqEDNTmQvcc6hlC+CnXWq\/PCtTtdeGGr0HNWFz7aeTcr\/eT1eqe41PVWJYxd62zkovwXqQ6Cv8vErm9r8MZNEctLVf9Ph4GwecSOjPSQu8V28e6chmRrFPq28btHJhhSBKDIBH0B726HEDilAWCtsGCx6MmjMNgaHqBkiygPl3UmHE1OrjOEiqoqsqDHGizYxrqGAXCOriL9jJUOoq3lO9UDQItpQGD3PtXZRapFHB51oSchwiWsDWA8Hl8Cpb2svA+WWO4AXJ7Q0Z1IJObq7sUBN\/UQLr0hrHfANSCaQihrobiBf+dC+QOxSNk4MrdEyN1VvRy6T+DdVbOp2wpcCyutN8yDahjtVZ3A4sS2ALwflYxhaK1n0C22QfL8dkuNRYZBlKxxRLj6gg3XNgj+w8beLOriHcv50gocJ8wsfP\/ASgfMDlRs8XzwfGPeThMr2QcpEqFprJmPSoSwSvMYfCcFX6nFlNPEfAGrUuC1GkhxQpspTnTfKysrFxl5qqqqZouy0mU81ZmApB6cp2vKlrFVk+Z7TGRuPOsXRZSNfGYmEO\/ofoXmOiKKS3Ga6wPYeRNxk9Vo49BONAIL1mUdIBa9bxPgn+zMOkAqIXDdQbp1sKAy6uWeyWd0Cyuk5LDh3pTtez\/NSVJqNwbh5kNZB8htkTrP1zHA9dM1nZ07nExmc50ZQGA7M+GxyLden9MA\/OLm\/KKe34xPoKAD7jPkAAAAAElFTkSuQmCC","width":22}
+%---
+%[text:image:1d94]
+%   data: {"align":"baseline","height":23,"src":"data:image\/png;base64,iVBORw0KGgoAAAANSUhEUgAAADoAAAA6CAYAAADhu0ooAAAEp0lEQVR42t2a208TQRTG67OJxvv9fiGoD6LGgPiCGo1G\/wljTKDl1pL00RuKIKCIIiKg6ItB8IJ38K4Panz13RcfSBBBQQHRek4ys\/kYpu1uu2132+Sku6eb6X7zmzlz5ux6PEn6lJaWbi0uLu4mGyLrJWvMz8+f4UmnjxA5QhZS7HNZWdnUdNE5hQR9EsLGioqKWuj7kRRbUlJSmS40D4Co4yD+rfAPEdW5rhdKQj4KQQM4J0n0DhjCVW4XuR9oHlV\/J98rSZVsnmuF0nz8oNIsLCxcFQwGp4vf86AjzrhV5D4dTTq+Tr8d0VD95fP5Frpx2L4XAgYlTQpMq+n8DxKm4+0wV6tdJZIo7YWbPwbi26QfqdL5S+H\/7SqqdMPvJE2iOFPOTUEzpM5bpEodUOMWkXs06yb7r6qZEc5dOn\/hKqqQCESiOYkqic4Ff62jRZKg3XCzJ0B8q0akjupzSZU6aZGTl5Q34kZ\/BgKB2ezzer3LOMcNJ5SpSvJIlY7POTWn3QUBpRzEt0QQqYvMzxxNlW7staRJQ3gO0Bw1IdSYz9Qx28Bf57QhuxNu7iSIbzYhclKEpvZ6HElVR5OOl5qkGZEqdcB5p9DMC0OzyYJIXaTuFr4R6rzFTkj3jK1WHDRDarSmdnPAX59qkbkQaU+B\/7IqglM7+g4qVqu5rhzaeeoIqpC2RaVJN52pGfYbdFRlW0iVrr2QcppkFSC+MUxUNSt0AlU6f5JSqpCuGcUtv9+\/JExZ05JQpErf2eC\/mOxIi4v6aRDfEGGdtCJUjeCPhW+Uk5BkCu2xQjNGoQZVWl+30Pk\/4W9I1tzM0RWfeVhFyXysCp0QyaHoPUZtLU\/G3OxWadIfL+Dilt1CMZrTtZuB6qVE7zezdUVnDv0mctlYhKpUHyaFKoR6SzTjEYr\/pVBtTDhNLDZz0m1ydxKrUHWdfiCp+ny+FYmgKUP8sHx8YJamDUK1VDnVtHs5wSFTDeLrLOw34xGqrtf3E0IVgoBRiiwoKJhvlqZNQjEubIKOb7Jr3TQaxeIyF6+sbMFsEKqu2\/ZShQZVmsPJFhqOKrVzxTaaWFTW7SNNCD3IlUI08h2KYXOO63eXpErtrYwnCHWpNDniWqVpsxlUacnLAhDNsa6bWTqaolIQSrFVwei6FxdVGlZ3baY5SNav2KCdVLlYblXkRqB5FnqvOlYKNgWjUJjsTEIZpzbXWIm0d9QCMlfneI\/oFKFKhoZgWs0GIP7zv+qDHu7BeOZVAoSqWZqEM+73+9eaodlpN80ECjXiBwLih87Rnoith4vrgGZlvJEyQUJVqrdNUaULOmRp0U6a8uUMssNo\/BDYBqHDnKlppt21qDTxwQ7vGhywbkbrxBrN1Bsny9DRbFcLxV6vdxad\/3C6UJyryvRrU7OgdfBjPYivcIHIkCYX79BSpZObLqYZkSq\/kicjaqZ04oMcrpS7SGRIzeJIy60JVDk6SZpcbRdDeRr5v2hyU6fbV\/l8VVlXm5lcX0wJsTveYpMVw14PzMOGNBQqE4h+3M8x5hti3UwH64S52y6D0YALA49Z6+P3hCXiDLH2fEsjgd+ZKr9wyRr\/A+UKAPezYlA8AAAAAElFTkSuQmCC","width":23}
+%---
